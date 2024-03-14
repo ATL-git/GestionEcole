b1bc891183c6ad47e608e5d72f344a1a4d30c839
xoxoATLxoxo <a.maureau13400@gmailcom>
2024-03-14 09:56:16 +0100

    ajout du css

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,25 +1,33 @@
 const promoContainer = document.querySelector('#promoContainer')
+const fromAddPromo = document.querySelector('#fromAddPromo')
+const buttonHidden = document.querySelector('#buttonHidden')
 
-async function getPromos(){
-    const response = await fetch ('http://146.59.242.125:3004/promos',{
+
+let namePromoChang = ""
+let startDateChang = ""
+let endDateChang = ""
+
+async function getPromos() {
+    const response = await fetch('http://146.59.242.125:3004/promos', {
         headers: {
-            "Authorization" : "Bearer bbd226c3-f867-474f-991a-8968f62a95bf"
+            "Authorization": "Bearer bbd226c3-f867-474f-991a-8968f62a95bf"
         }
     })
     const data = await response.json()
     return data
 }
 
-async function displayPromo(){
+async function displayPromo() {
     const promos = await getPromos()
     console.log(promos);
     promoContainer.innerHTML = ""
 
     promos.forEach(promo => {
+        
         let promoDiv = document.createElement('div')
         promoContainer.appendChild(promoDiv)
         promoDiv.classList.add('promo')
-        let titrePromo =document.createElement('h2')
+        let titrePromo = document.createElement('h2')
         promoDiv.appendChild(titrePromo)
         titrePromo.classList.add('titrePromo')
         titrePromo.textContent = promo.name
@@ -29,48 +37,106 @@ async function displayPromo(){
         let delet = document.createElement('button')
         divButton.appendChild(delet)
         delet.textContent = 'supprimer promo'
-        let modif = document.createElement('a')
-        modif.href = "/assets/pages/student.html?id="+promo._id
+        let detail = document.createElement('a')
+        detail.href = "/assets/pages/student.html?id=" + promo._id
+        divButton.appendChild(detail)
+        detail.textContent = 'details promo'
+        let modif = document.createElement('button')
         divButton.appendChild(modif)
-        modif.textContent = 'modifier promo'
-        delet.addEventListener('click',()=>{
+        modif.textContent = 'modifier la promo'
+        modif.addEventListener('click', () => {
+           
+                let changDiv = document.createElement('div')
+                let changPromoName = document.createElement('input')
+                let changStartDate = document.createElement('input')
+                changStartDate.type = "date"
+                let changEndDate = document.createElement('input')
+                changEndDate.type = "date"
+                let validName = document.createElement('button')
+                validName.textContent = "Valider changement"
+                promoDiv.appendChild(changDiv)
+                changDiv.appendChild(validName)
+                changDiv.appendChild(changPromoName)
+                changDiv.appendChild(changStartDate)
+                changDiv.appendChild(changEndDate)
+                changPromoName.placeholder = promo.name
+
+            
+           
+            validName.addEventListener('click', () => {
+                namePromoChang = changPromoName.value
+                startDateChang = changStartDate.value
+                endDateChang = changEndDate.value
+                changPromo(promo._id)
+            })
+
+        })
+        delet.addEventListener('click', () => {
             deletePromo(promo._id)
         })
-        
+
     });
 }
 
-async function deletePromo(promoId){
+async function deletePromo(promoId) {
     const response = await fetch("http://146.59.242.125:3004/promos/" + promoId, {
         method: "DELETE",
         headers: {
-            "Authorization" : "Bearer bbd226c3-f867-474f-991a-8968f62a95bf"
-            
+            "Authorization": "Bearer bbd226c3-f867-474f-991a-8968f62a95bf"
+
         }
     })
     const data = await response.json()
     displayPromo()
 }
 
-async function addPromo(){
- 
+async function addPromo() {
+
     const body = {
-        "name" : document.querySelector('#name').value ,
-        "startDate" :document.querySelector('#startDate').value,
-        "endDate" :document.querySelector('#endDate').value
+        "name": document.querySelector('#name').value,
+        "startDate": document.querySelector('#startDate').value,
+        "endDate": document.querySelector('#endDate').value
     }
-const response = await fetch("http://146.59.242.125:3004/promos", {
-    method: "POST",
-    headers: {
-        "Authorization" : "Bearer bbd226c3-f867-474f-991a-8968f62a95bf",
-        "Content-type" : "Application/json"
-    },
-    body : JSON.stringify(body)
-})
-const data = await response.json()
-displayPromo()
+    const response = await fetch("http://146.59.242.125:3004/promos", {
+        method: "POST",
+        headers: {
+            "Authorization": "Bearer bbd226c3-f867-474f-991a-8968f62a95bf",
+            "Content-type": "Application/json"
+        },
+        body: JSON.stringify(body)
+    })
+    const data = await response.json()
+    displayPromo()
 }
 
+async function changPromo(promoId) {
 
+    const body = {
+        "name": namePromoChang,
+        "startDate": startDateChang,
+        "endDate": endDateChang
+    }
+    const response = await fetch("http://146.59.242.125:3004/promos/" + promoId, {
+        method: "PUT",
+        headers: {
+            "Authorization": "Bearer bbd226c3-f867-474f-991a-8968f62a95bf",
+            "Content-type": "Application/json"
+        },
+        body: JSON.stringify(body)
+    })
+    const data = await response.json()
+    displayPromo()
+}
+
+function hiddenPromo() {
+
+    promoContainer.classList.toggle("hidden")
+    fromAddPromo.classList.toggle("hidden")
+    if (fromAddPromo.classList == "hidden") {
+        buttonHidden.textContent = "Ajouter une Promo"
+    } else if (fromAddPromo.classList != "hidden") {
+        buttonHidden.textContent = "retour"
+    }
+}
 
 displayPromo()
--- assets/javascript/studdent.js
+++ assets/javascript/studdent.js
@@ -1,4 +1,9 @@
 const studentContainer = document.querySelector('#studentContainer')
+const fromAddStudent = document.querySelector('#fromAddStudent')
+const buttonHidden = document.querySelector('#buttonHidden')
+let namechang = ""
+let lastNameChang = ""
+let newAge = ""
 
 function getPromoId() {
     let url = window.location.href
@@ -30,15 +35,42 @@ async function displayStudents() {
         let titreStudent = document.createElement('p')
         studentDiv.appendChild(titreStudent)
         titreStudent.classList.add('titreStudent')
-        titreStudent.textContent = `${student.firstName}  ${student.lastName}`
+        titreStudent.textContent = `${student.firstName}  ${student.lastName} /${student.age}ans`
         let divButton = document.createElement('div')
         studentDiv.appendChild(divButton)
         divButton.classList.add('divButton')
         let delet = document.createElement('button')
         divButton.appendChild(delet)
         delet.textContent = 'supprimer éleve'
+        let modif = document.createElement('button')
+        divButton.appendChild(modif)
+        modif.textContent = 'modifier éleve'
         delet.addEventListener('click', () => {
             deleteStudent(student._id)
+        })
+        modif.addEventListener('click', () => {
+           
+            let changDiv = document.createElement('div')
+            let changFirstName = document.createElement('input')
+            let changLastName = document.createElement('input')
+            let changAge = document.createElement('input')
+            let validName = document.createElement('button')
+            validName.textContent = "Valider nouveau nom"
+            studentDiv.appendChild(changDiv)
+            changDiv.appendChild(validName)
+            changDiv.appendChild(changFirstName)
+            changDiv.appendChild(changLastName)
+            changDiv.appendChild(changAge)
+            changFirstName.value = student.firstName
+            changLastName.value = student.lastName
+            changAge.value = student.age
+            validName.addEventListener('click', () => {
+                namechang =  changFirstName.value
+                lastNameChang = changLastName.value
+                newAge = changAge.value
+                changStudent(student._id)
+            })
+
         })
 
     });
@@ -73,5 +105,35 @@ async function addStudent() {
     })
     const data = await response.json()
     displayStudents()
+}
+
+async function changStudent(studentId) {
+
+    const body = {
+        "firstName": namechang,
+        "lastName": lastNameChang,
+        "age": newAge
+    }
+    const response = await fetch(`http://146.59.242.125:3004/promos/${getPromoId()}/students/${studentId}`, {
+        method: "PUT",
+        headers: {
+            "Authorization": "Bearer bbd226c3-f867-474f-991a-8968f62a95bf",
+            "Content-type": "Application/json"
+        },
+        body: JSON.stringify(body)
+    })
+    const data = await response.json()
+    displayStudents()
+}
+
+function hiddenStudent(){
+
+    studentContainer.classList.toggle("hidden")
+    fromAddStudent.classList.toggle("hidden")
+    if (fromAddStudent.classList == "hidden") {
+        buttonHidden.textContent = "Ajouter un éléve"
+    }else if (fromAddStudent.classList != "hidden") {
+        buttonHidden.textContent = "retour"
+    }
 }
 displayStudents()
